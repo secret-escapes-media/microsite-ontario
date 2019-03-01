081aea8f9cbd00651bfc83b4aa9c67718b69b357
Birgit <birgit.voslamber@secretescapes.com>
2019-03-01 15:34:47 +0100

    checklist updates

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,1 +1,30 @@
 // general js for the project that wouldn't be a reuseable component
+
+// Get the modal
+var modal = document.getElementById('bv-myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("bv-myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("bv-close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+    $('body').addClass('bv-disable-scroll');
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+      $('body').removeClass('bv-disable-scroll');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+        $('body').removeClass('bv-disable-scroll');
+  }
+}
